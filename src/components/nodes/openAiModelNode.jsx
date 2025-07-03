5eb73df17ec5182dc08308646efe894747196415
Dhawal Sarin <dhawal12sarin@gmail.com>
2025-07-03 17:22:22 +0530

    CSS changes to the nodes.

--- src/components/nodes/openAiModelNode.jsx
+++ src/components/nodes/openAiModelNode.jsx
@@ -6,7 +6,7 @@ function OpenAiModelNode(props) {
  
   return (
     <div style={{ display:'flex', border:'1px solid #c3c9d5', width:50, height:50, padding:5, justifyContent:'center', alignItems:'center', color:'#c3c9d5', backgroundColor:'#2c2d3a', borderRadius:'100%'}}>
-      <Handle type="target" position="top" style={{ background: '#c3c9d5', width: 10, height:10 }} />
+      <Handle type="target" position="top" style={{ background: '#8c8ea6', width: 10, height:10, borderRadius:0, border:'none', transform:'rotate(45deg)', top:-4, left:25 }} />
       <div style={{display:'flex', justifyContent:'center', alignItems:'center',flexDirection:'column', color:'#c3c9d5', scale:0.6}}>
       <OpenAiModel/>
       </div>
